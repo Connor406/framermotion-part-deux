d42cef2a93f418aa6c5bc09cf790be5fcb753a9f
Connor Mahan <mahan406@gmail.com>
2021-07-09 11:07:11 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+import "./styles.css";
+
+export default function App() {
+  const [isToggled, setIsToggled] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1 className="fake-logo">Level Up</h1>
       </header>
+      <main className="layout">
+        <button onClick={() => setIsToggled(!isToggled)}>Toggle</button>
+        {isToggled && (
+          <div className="card">
+            <h4>Post Number one</h4>
+            <p>this is inside the card</p>
+          </div>
+        )}
+      </main>
     </div>
   );
 }
-
-export default App;
